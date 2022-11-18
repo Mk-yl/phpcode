8bffa63ad97efcc1f65cd5f3e0b624b61a2b9b37
Mkl-01 <118522076+Mkl-01@users.noreply.github.com>
2022-11-18 11:23:31 +0100

    Delete Mika Ly directory

--- Mika Ly/album_photo/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class config
-{
-    const SERVEUR="bkdocggbckko1zdky3fs-mysql.services.clever-cloud.com";
-    const BDD="bkdocggbckko1zdky3fs";
-    const UTILISATEUR="upob1y3y18kozvfz";
-    const MOTDEPASSE="t1u8jnyTCJUxRneFT8bE";
-}
